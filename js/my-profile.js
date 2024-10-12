46bc8b228867acd387d23bc270f3da4ba5e72d63
guillerodriguez09 <rodriguezguillermina816@gmail.com>
2024-10-12 09:39:52 -0300

    agregue la validacion y se guardan los datos del usuario

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,2 +1,66 @@
-if (localStorage.getItem("usuario") && localStorage.getItem("contraseña")){ document.getElementById("user").innerHTML = "Cliente: " + username;
-}
+/*if (localStorage.getItem("usuario") && localStorage.getItem("contraseña")) {
+    document.getElementById("user").innerHTML = "Cliente: " + username;
+}*/
+document.addEventListener('DOMContentLoaded', () => {
+
+    const usuario = localStorage.getItem('username');
+    if (!usuario) {
+        window.location.href = 'login.html';
+        return;
+    }
+    let inputEmail = document.getElementById('email'); // saco la casillas de los input
+    let inputNombre = document.getElementById('nombre');
+    let inputSegundoN = document.getElementById('segundo-nombre');
+    let inputApellido = document.getElementById('apellido');
+    let inputSegundoA = document.getElementById('segundo-apellido');
+    let inputTel = document.getElementById('telefono');
+    inputEmail.value = usuario;
+
+    console.log(inputApellido);
+    
+
+
+    let datosPerfil = JSON.parse(localStorage.getItem(`datosDeUsuarios${usuario}`)) || [];
+    if (datosPerfil.length > 0) {
+        let perfilGuardado = datosPerfil[datosPerfil.length - 1]; // Último perfil guardado
+        inputNombre.value = perfilGuardado.nombreU || "";
+        inputSegundoN.value = perfilGuardado.segundoNombreU || "";
+        inputSegundoA.value = perfilGuardado.segundoApellidoU || "";
+        inputApellido.value = perfilGuardado.apellidoU || "";
+        inputTel.value = perfilGuardado.telefonoU || "";
+    }
+
+    let form= document.getElementById('formulario-perfil');
+    
+                form.addEventListener('submit', function (event) {
+                    if (!form.checkValidity()) {
+                        event.preventDefault() //evita que se envie
+                        event.stopPropagation()
+                    } else {
+                        let nuevoPerfil = {
+                            nombreU: inputNombre.value,
+                            segundoNombreU: inputSegundoN.value,
+                            apellidoU: inputApellido.value,
+                            segundoApellidoU: inputSegundoA.value,
+                            telefonoU: inputTel.value,
+                            emailU: usuario
+                        }; //crea un objeto para guardar en lstorage con los datos que ingreso el usuario
+                            datosPerfil.push(nuevoPerfil); // los pushea al arreglo de los datos
+                            localStorage.setItem(`datosDeUsuarios${usuario}`, JSON.stringify(datosPerfil)); //los convierte en un objeto json para guardar en local storage
+                            }
+                        form.classList.add('was-validated');
+
+                            
+                        }, false)
+
+                    })
+                
+
+               
+            
+
+         
+            
+
+
+
