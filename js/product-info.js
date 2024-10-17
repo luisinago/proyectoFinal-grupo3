8f3e82460b1d1754f2ded8ccaf7d78a09ad08e6b
luisinago <luisinago@gmail.com>
2024-10-17 18:51:18 -0300

    Mejora en código puntuar con estrellas (ahora usamos classList.add y classList.remove) + Limpié comentarios de pruebas de código anteriores

--- js/product-info.js
+++ js/product-info.js
@@ -2,53 +2,10 @@ if (localStorage.getItem("usuario") && localStorage.getItem("contraseña")){ doc
 }
 
 
-
-
-/* let productoID = localStorage.getItem("productoID");
-let categoriaID = localStorage.getItem("categoriaID");
-
-// Categorías con las URL de los JSON
-let categoriasURL = {
-  101: "https://japceibal.github.io/emercado-api/cats_products/101.json",  // Autos
-  102: "https://japceibal.github.io/emercado-api/cats_products/102.json",  // Juguetes
-  103: "https://japceibal.github.io/emercado-api/cats_products/103.json",  // Muebles
-  105: "https://japceibal.github.io/emercado-api/cats_products/105.json"   // Computadoras
-};
-
-// fetch según categoría seleccionada
-let fetchProductosInfo = async () => {
-  if (categoriaID && categoriasURL[categoriaID]) {
-    let response = await fetch(categoriasURL[categoriaID]);
-    let datos = await response.json();
-
-    // Buscamos el producto por su ID dentro del JSON
-    let producto = datos.products.find(p => p.id == productoID);
-
-    if (producto) {
-      // Actualizamos el contenido en el HTML
-      document.querySelector('h1').innerText = producto.name;
-      document.querySelector('.imagen-producto img').src = producto.image;
-      document.querySelector('.descripcion-producto p').innerText = producto.description;
-
-      // Actualizamos la categoría y las unidades vendidas
-      document.querySelector('.categoria-producto').innerText = datos.catName;  // Nombre de la categoría
-      document.querySelector('.unidades-vendidas').innerText = `Unidades vendidas: ${producto.soldCount}`;  
-    } else {
-      console.log('Producto no encontrado');
-    }
-  } else {
-    console.log('Categoría no encontrada');
-  }
-};
-
-fetchProductosInfo();  */
-
 let productoID = localStorage.getItem("productoID");
 let categoriaID = localStorage.getItem("categoriaID");
 
-/*let productosURL = {
-  `https://japceibal.github.io/emercado-api/cats_products/${categoriaId}.json`;
-}*/
+
 // Categorías con las URL de los JSON
 let categoriasURL = {
     101: "https://japceibal.github.io/emercado-api/cats_products/101.json",  // Autos
@@ -146,23 +103,7 @@ let fetchOpiniones = async (productoID) => {
 
     fetchOpiniones(productoID);
 
-    /*function generarEstrellas(puntaje) {
-      let estrellas = '';
-      let rating = puntaje;
-  
-  
-      for (let i = 1; i <= 5; i++) {
-          if (i <= rating) {
-              estrellas += `<span class="fa fa-star checked"></span>`;
-          } else {
-              estrellas += `<span class="fa fa-star"></span>`;
-          }
-      }
-  
-  
-      return estrellas;
-  }
-      */
+ 
 
 //Espacio para que el usuario puntúe e ingrese comentario
 const stars = document.querySelectorAll('#star-rating span');
@@ -170,9 +111,9 @@ stars.forEach((star, index) => {
     star.addEventListener('click', () => {
         stars.forEach((s, i) => {
             if (i <= index) {
-                s.classList += 'fa fa-star checked';
+                s.classList.add('checked');
             } else {
-                s.classList = 'fa fa-star';
+                s.classList.remove('checked');
             }
         });
     });
